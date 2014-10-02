f507c0be5677dc7eb13691621544eab2b195805f
Ari Lipsey <maximulus@gmail.com>
2014-10-02 18:32:14 -0400

    Request for Authorization required for ios8

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/maximulus/tourvu/tourvuapp/beaconpart/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/maximulus/tourvu/tourvuapp/beaconpart/platforms/android/AndroidManifest.xml \
+/Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/BeaconTest-debug-unaligned.apk.d
+++ platforms/android/ant-build/BeaconTest-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/maximulus/tourvu/tourvuapp/beaconpart/platforms/android/ant-build/BeaconTest-debug-unaligned.apk : \
-/Users/maximulus/tourvu/tourvuapp/beaconpart/platforms/android/ant-build/BeaconTest.ap_ \
-/Users/maximulus/tourvu/tourvuapp/beaconpart/platforms/android/ant-build/classes.dex \
+/Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/ant-build/BeaconTest-debug-unaligned.apk : \
+/Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/ant-build/BeaconTest.ap_ \
+/Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/ant-build/classes.dex \
 /Users/maximulus/TourVu/tourvuapp/beaconpart/platforms/android/CordovaLib/ant-build/classes.jar \
